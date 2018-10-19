bd30df31913722b74eeb7968eb5d4901c4773be5
Brian Nelson <41879246+chillbnel@users.noreply.github.com>
2018-10-19 12:46:19 -0700

    Built JsonConversion method in Program.cs

--- Lab08-LINQinManhatten/Program.cs
+++ Lab08-LINQinManhatten/Program.cs
@@ -1,4 +1,9 @@
 ﻿using System;
+using Lab08_LINQinManhatten.Classes;
+using System.Collections.Generic;
+using System.IO;
+using Newtonsoft.Json;
+using System.Linq;
 
 namespace Lab08_LINQinManhatten
 {
@@ -7,6 +12,22 @@ namespace Lab08_LINQinManhatten
         static void Main(string[] args)
         {
             
+        }
+
+        static void JsonConversion()
+        {
+            string path = "../../data.json";
+            string text = "";
+            using (StreamReader sr = File.OpenText(path))
+            {
+                text = sr.ReadToEnd();
+            }
+
+            IEnumerable<Properties> brooklynProperties = JsonConvert.DeserializeObject<List<Properties>>(text);
+
+            var someName = brooklynProperties.Where(x => x.neighborhood);
+
+
         }
     }
 }
