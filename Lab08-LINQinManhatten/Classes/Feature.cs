205c324ab97aa6119cbe5a1027cfcf52de367287
Brian Nelson <41879246+chillbnel@users.noreply.github.com>
2018-10-19 12:21:06 -0700

    JSON file imported and JSON classes setup

---
+++ Lab08-LINQinManhatten/Classes/Feature.cs
@@ -0,0 +1,16 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lab08_LINQinManhatten.Classes
+{
+    class Feature
+    {
+        [JsonProperty("type")]
+        public string type { get; set; }
+
+        [JsonProperty("properties")]
+        public Properties properties { get; set; }
+    }
+}
